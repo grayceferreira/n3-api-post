5272dfe5fa14afc26ecf27f6001c2d60d287e37c
Grayce Ferreira <grayce.ferreira@gmail.com>
2019-11-21 20:20:35 -0300

    aplicar bodyParser

--- servidor-contato/src/routes/contatosRoute.js
+++ servidor-contato/src/routes/contatosRoute.js
@@ -4,9 +4,9 @@ const bodyParser = require("body-parser")
 const controller = require("../controller/contatosController")
 
 router.get("/", controller.getAll)
-router.post("/criar", bodyParser.json(), controller.add)
+router.post("/criar", controller.add)
 router.get("/nome/:nome", controller.getNome)
 router.get("/id/:id", controller.getById)
-router.patch("/atualizar/:id", bodyParser.json(), controller.updateById)
+router.patch("/atualizar/:id", controller.updateById)
 
 module.exports = router;
